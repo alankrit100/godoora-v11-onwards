653cc35452ccd39824bdb64494817f95688a32fd
Alankrit Bajoria <alankritbajoria100@gmail.com>
2025-07-09 02:21:29 +0530

    angular version 13

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -4,21 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 import { LoginPage } from './login.page';
-import { UiSwitchModule } from 'ngx-ui-switch';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    UiSwitchModule.forRoot({
-      size: 'medium',
-      color: '#d0d4d6',
-      switchColor: '#92989c',
-      defaultBgColor: 'rgb(236, 240, 245)',
-      checkedLabel: 'Hotel Guest',
-      uncheckedLabel: 'Visitor'
-    }),
     LoginPageRoutingModule
   ],
   declarations: [LoginPage]
