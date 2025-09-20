663f02e8d42f254795a826b48ac8e3237883a1aa
Aayush Mishra. <manofganges@gmail.com>
2025-09-20 05:57:09 +0530

    define the component's logic, allowing it to accept an array of images and handle the modal dismissal

--- src/app/components/carousel-in-modal/carousel-in-modal.component.ts
+++ src/app/components/carousel-in-modal/carousel-in-modal.component.ts
@@ -1,24 +1,28 @@
-import { Component, Input, OnInit, ViewChild } from "@angular/core";
-import { ModalController } from "@ionic/angular";
+import { Component, Input } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+interface CarouselImage {
+  src: string;
+  alt: string;
+  caption?: string;
+}
 
 @Component({
-    selector: 'app-carousel-in-modal',
-    templateUrl: './carousel-in-modal.component.html',
-    styleUrls: ['./carousel-in-modal.component.scss'],
+  selector: 'app-carousel-in-modal',
+  templateUrl: './carousel-in-modal.component.html',
+  styleUrls: ['./carousel-in-modal.component.scss'],
 })
+export class CarouselInModalComponent {
+    
+  @Input() images: CarouselImage[] = [];
+    
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+  };
 
-export class CarouselInModalComponent implements OnInit {
-
-    @Input() imgPath: string;
-
-    constructor(
-        private modalCtrl: ModalController
-    ){}
-
-    ngOnInit(){}
-
-    async dismiss(flag = false) {
-        await this.modalCtrl.dismiss({ flag });
-    }
-
+  constructor(private modalCtrl: ModalController) {}
+    
+  async dismiss() {
+    await this.modalCtrl.dismiss();
+  }
 }
